b4095d0f4739802786113005aed98f30647a4c9a
prashanth poshettiwar <poshettiwarp@gmail.com>
2025-08-28 10:43:28 +0530

    Implemented Logging For AuthController - createUser()

--- src/main/java/com/greenWarrior/controller/AuthController.java
+++ src/main/java/com/greenWarrior/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.greenWarrior.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,20 +17,21 @@ import com.greenWarrior.service.UserService;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+	private static Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
+
 	@Autowired
 	private UserService userService;
-	
+
 	@PostMapping("/register")
 	public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDTO requestDTO) {
-
-		return ResponseEntity.created(null).body(userService.createUser(requestDTO));
+		LOGGER.info("Start : AuthController -------> createUser() method : " + requestDTO);
+		return new ResponseEntity<UserResponseDto>(userService.createUser(requestDTO), HttpStatus.CREATED);
 	}
-	
+
 	@PostMapping("/login")
 	public ResponseEntity<String> userLogin() {
 
 		return ResponseEntity.ok("Login Successfull..");
 	}
-	
-	
+
 }
